8c485bcbd4b019b0a2fa872f9f5869e6ae925b54
Antonio Palmeira <av.palmeira@gmail.com>
2020-11-05 17:09:15 -0300

    try: new database config for production

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 module.exports = {
-  username: "geopoints",
-  password: "987654",
-  database: "geopoints",
-  host: "127.0.0.1",
-  dialect: "postgres"
+  development: {
+    username: "geopoints",
+    password: "987654",
+    database: "geopoints",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres"
+  }
 }
