c4534b459dcb19cb1498bb694db43da4cca70d68
Antonio Palmeira <av.palmeira@gmail.com>
2020-11-06 19:16:45 -0300

    setup test configs

--- config/database.js
+++ config/database.js
@@ -6,6 +6,13 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres"
   },
+  test: {
+    username: "geopoints",
+    password: "987654",
+    database: "geopoints_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres"
